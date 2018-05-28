3ec7efa73243c4370714172f3f8afdcebc3537f1
speedmann <speedmann@speedmann.de>
2018-05-28 20:42:50 +0200

    Add image url

--- api.py
+++ api.py
@@ -17,17 +17,17 @@ def api_get_comment(id):
     else:
         return False
 
-def api_create_post(id, text, gender):
+def api_create_post(id, text, gender, image):
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    data = {"id":id,"text":text, "gender":gender}
+    data = {"id":id,"text":text, "gender":gender , "image": "/images/{}/{}'.format(gender, image.split('/')[-1])}
     url = 'http://jodel1.tuxcall.de:5000/api/posts'
     r = requests.post(url, data=json.dumps(data) , headers=headers)
 
-def api_create_comment(post_id, comment_id, text, gender):
+def api_create_comment(post_id, comment_id, text, gender, image):
     posts = api_get_post(post_id)
     comment = api_get_comment(comment_id)
     if posts and not comment:
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        data = {"id":comment_id, "text":text, "post_id":post_id, "gender":gender}
+        data = {"id":comment_id, "text":text, "post_id":post_id, "gender":gender, "image": "/images/{}/{}'.format(gender, image.split('/')[-1])}
         url = 'http://jodel1.tuxcall.de:5000/api/comments'
         r = requests.post(url, data=json.dumps(data),headers=headers)
